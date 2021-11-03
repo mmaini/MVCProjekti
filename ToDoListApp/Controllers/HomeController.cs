32145a01c06831119957369dfa9d3862cc3a0c6f
mmaini <mariza.maini@itsistemi.com>
2021-11-03 14:07:59 +0100

    TodoApp
    Dorada

--- ToDoListApp/Controllers/HomeController.cs
+++ ToDoListApp/Controllers/HomeController.cs
@@ -13,105 +13,101 @@ namespace ToDoListApp.Controllers
     public class HomeController : Controller
     {
 
-        private readonly AppDbContext _db;
-        public HomeController(AppDbContext db)
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext ctx)
         {
-            _db = db;
+            _context = ctx;
         }
 
         public IActionResult Index(string id)
         {
             var model = new ToDoViewModel();
-            //punimo model sa svim podacima koje ćemo proslijediti view-u
+            //punimo model podacima
             model.Filters = new Filters(id);
-            model.Categories = _db.Categories.ToList();
-            model.Statuses = _db.Statuses.ToList();
+            model.Categories = _context.Categories.ToList();
+            model.Statuses = _context.Statuses.ToList();
             model.DueFilters = Filters.DueFilterValues;
 
-            IQueryable<ToDo> query = _db.ToDos.Include(c => c.Category).Include(s => s.Status);
-            //ako imamo filtere, primijeni ih na upit
-            if(model.Filters.HasCategory)
-            {
-                query = query.Where(x => x.CategoryId == model.Filters.CategoryId);
-            }
+            IQueryable<ToDo> query = _context.ToDos.Include(c => c.Category).Include(s => s.Status);
+
+            //ako je definirano neko filtriranje, uključi ga
+            if (model.Filters.HasCategory)
+                query = query.Where(t => t.CategoryId == model.Filters.CategoryId);
+
             if (model.Filters.HasStatus)
-            {
-                query = query.Where(x => x.StatusId == model.Filters.StatusId);
-            }
+                query = query.Where(t => t.StatusId == model.Filters.StatusId);
+
             if (model.Filters.HasDue)
             {
-                //definiraj koji je današnji datum
                 var today = DateTime.Today;
-                //ovisno o tome da li korisnik traži obaveze u prošlosti/sadašnjosti/budućnosti primijeni filtriranje
-                if(model.Filters.IsPast)
-                    query = query.Where(x => x.DueDate <today);
-                else if(model.Filters.IsFuture)
-                    query = query.Where(x => x.DueDate > today);
-                else if(model.Filters.IsToday)
-                    query = query.Where(x => x.DueDate == today);
+
+                if (model.Filters.IsPast)
+                    query = query.Where(t => t.DueDate < today);
+                else if (model.Filters.IsFuture)
+                    query = query.Where(t => t.DueDate > today);
+                else if (model.Filters.IsToday)
+                    query = query.Where(t => t.DueDate == today);
             }
 
-            //dohvati iz baze 
             var tasks = query.OrderBy(t => t.DueDate).ToList();
             model.Tasks = tasks;
             return View(model);
         }
 
-
         [HttpGet]
         public IActionResult Add()
         {
             var model = new ToDoViewModel();
-            model.Categories = _db.Categories.ToList();
-            model.Statuses = _db.Statuses.ToList();
+            model.Categories = _context.Categories.ToList();
+            model.Statuses = _context.Statuses.ToList();
+
             return View(model);
         }
 
         [HttpPost]
         public IActionResult Add(ToDoViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                _db.ToDos.Add(model.CurrentTask);
-                _db.SaveChanges();
+                _context.ToDos.Add(model.CurrentTask);
+                _context.SaveChanges();
                 return RedirectToAction("Index", "Home");
-            }
-
-            model.Categories = _db.Categories.ToList();
-            model.Statuses = _db.Statuses.ToList();
-            return View(model);
-
-        }    
-
-        [HttpPost]
-        //id su filteri, tako da kad ponovno učitamo zadržimo filtere koje je korisnik odabrao
-        public IActionResult EditDelete([FromRoute] string id, ToDo selected)
-        {
-            if(selected.StatusId == null)
-            {
-                _db.ToDos.Remove(selected);
             }
             else
             {
-                //objekt kojeg dobijemo od view-a ima novi status
-                string newStatusId = selected.StatusId;
-                //dohvati objekt iz baze (on još uvijek ima staru vrijednost statusa)
-                //dodijelimo mu novi status
-                selected = _db.ToDos.Find(selected.Id);
-                selected.StatusId = newStatusId;
-                _db.ToDos.Update(selected);              
+                model.Categories = _context.Categories.ToList();
+                model.Statuses = _context.Statuses.ToList();
+                return View(model);
             }
-            _db.SaveChanges();
-            //proslijeđujemo i filtere
-            return RedirectToAction("Index", "Home", new { ID = id } );
         }
 
         [HttpPost]
-        //metoda kada korisnik odabere filtere
-        public IActionResult Filter(string[] filters)
+        public IActionResult EditDelete([FromRoute] string id, ToDo selected)
         {
-            //iz niza stringova kreiramo jedan string
-            string id = string.Join('-', filters);
+            if (selected.StatusId == null)
+                _context.ToDos.Remove(selected);
+            else
+            {
+                //dohvati novi status iz objekta kojeg nam je poslao view
+                string newStatusId = selected.StatusId;
+                //dohvati objekt iz baze koji još ima stari status
+                selected = _context.ToDos.Find(selected.Id);
+                //postavi mu novi status
+                selected.StatusId = newStatusId;
+                _context.ToDos.Update(selected);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home", new { ID = id });
+        }
+
+        [HttpPost]
+        public IActionResult Filter(string[] filter)
+        {
+            //spajamo niz stringova u jedan string
+            string id = string.Join('-', filter);
             return RedirectToAction("Index", "Home", new { ID = id });
         }
     }
