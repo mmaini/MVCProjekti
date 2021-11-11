09db2d36c3dd683ad3206b53068b32c338bb127f
mmaini <mariza.maini@itsistemi.com>
2021-11-11 09:06:45 +0100

    Sitne prepravke

--- DevConferenceApp/Models/WebinarInvite.cs
+++ DevConferenceApp/Models/WebinarInvite.cs
@@ -10,17 +10,21 @@ namespace DevConferenceApp.Models
     public class WebinarInvite
     {
         [Required(ErrorMessage = "Molimo unesite ime")]
+        [Display(Name="Ime")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Molimo unesite email")]
         [EmailAddress]
+        [Display(Name = "Email adresa")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Molimo unesite telefon")]
         [Phone]
+        [Display(Name = "Broj telefona")]
         public string Phone { get; set; }
 
         [Required(ErrorMessage = "Molimo navedite da li ćete učestvovati")]
+        [Display(Name = "Dolazi")]
         public bool? WillJoin { get; set; }
 
     }
